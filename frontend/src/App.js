52cdf7df0efa2c3fde00bd3a31db5fb7fa50cc36
Marian Angelov <50790794+marianangelov@users.noreply.github.com>
2020-08-15 11:35:24 +0300

    Footer improvment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -95,7 +95,7 @@ function App() {
             <Route path="/" exact={true} component={HomeScreen} />
           </div>
         </main>
-        <footer className="footer">All right reserved.</footer>
+        <footer className="footer">SoftUni project. By M.Angelov</footer>
       </div>
     </BrowserRouter>
   );
